02224ea650fe28625db459063041729b733642c2
Jakub Michalski <robertmenel@gmail.com>
2023-08-10 22:44:26 +0200

    Added MVC scripts and Zenject dependency

---
+++ Assets/Scripts/UI/ForkliftUIView.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+    public class ForkliftUIView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text speedText;
+        [SerializeField] private TMP_Text forkPositionText;
+        [SerializeField] private TMP_Text horizontalInputText;
+        [SerializeField] private TMP_Text verticalInputText;
+        [SerializeField] private TMP_Text isObjectOnForkText;
+        
+        private ForkliftUIController forkliftUIController;
+        
+        [Inject]
+        private void Init(ForkliftUIController _forkliftUIController)
+        {
+            forkliftUIController = _forkliftUIController;
+        }
+        
+        private void Update()
+        {
+            speedText.text = forkliftUIController.Speed.ToString("F2");
+            forkPositionText.text = forkliftUIController.ForkPosition.ToString("F2");
+            horizontalInputText.text = forkliftUIController.HorizontalInput.ToString("F2");
+            verticalInputText.text = forkliftUIController.HorizontalInput.ToString("F2");
+            isObjectOnForkText.text = forkliftUIController.IsObjectOnFork ? "Yes" : "No";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "UIInstaller", menuName = "Installers/UIInstaller")]
+public class UIInstaller : ScriptableObjectInstaller<UIInstaller>
+{
+    [SerializeField] private ForkliftUIController forkliftUIController;
+    public override void InstallBindings()
+    {
+        Container.Bind<ForkliftUIController>().FromInstance(forkliftUIController).AsSingle();
+    }
+}
